6d7d62f95fa6fc9c4646b003e4f0a7474371c104
Wade Allen <wade.allen11@gmail.com>
2019-08-20 17:01:56 -0700

    Made new card classes that extend the Chance Card class.

--- src/main/java/com/codingdojo/monopoly/models/cards/AdvanceToUtilityCard.java
+++ src/main/java/com/codingdojo/monopoly/models/cards/AdvanceToUtilityCard.java
@@ -5,7 +5,6 @@ import com.codingdojo.monopoly.models.Player;
 public class AdvanceToUtilityCard extends ChanceCard {
 	private String name = "Advance to nearest utility";
 	
-	@Override
 	public void action(Player player) {
 		if(player.getCurrentLocation() > 12 && player.getCurrentLocation() < 28) {
 			player.setCurrentLocation(28);
@@ -13,6 +12,6 @@ public class AdvanceToUtilityCard extends ChanceCard {
 		else {
 			player.setCurrentLocation(12);
 		}
-		if ()
+//NEED TO ADD LOGIC FOR CHECKING WHETHER OR NOT THE UTILITY IS OWNED.
 	}
 }
