ecee79a8fa362e5842fd3f337298b6da4cf5d9ce
Michael Kemper <michaelkemper31@gmail.com>
2019-08-29 16:26:13 -0700

    Added activity logging for landing on Utilities

--- src/main/java/com/codingdojo/monopoly/models/Utility.java
+++ src/main/java/com/codingdojo/monopoly/models/Utility.java
@@ -16,6 +16,13 @@ public class Utility extends Property {
 	@Override
 	public int getRentCost() {
 		int roll = Game.rollDie() + Game.rollDie();
+		String activity = new StringBuilder(Game.getCurrentPlayer().getName())
+				.append(" landed on ")
+				.append(this.getName())
+				.append(" and rolled ")
+				.append(Integer.toString(roll))
+				.toString();
+		Game.addActivityLogItem(activity);
 		boolean ownsAll = Game.ownsFullSet(this.getOwner(), this.getSet());
 		int multiple = 4;
 		if(ownsAll) multiple = 10;
