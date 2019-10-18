840bae592fe0638c3380421b54833ade9030fd30
phatluong123 <phat.luong1128@gmail.com>
2019-10-18 13:08:10 -0700

    add reset msg

---
+++ src/main/java/com/codingdojo/monopoly/scmodels/ResetMessage.java
@@ -0,0 +1,27 @@
+package com.codingdojo.monopoly.scmodels;
+
+public class ResetMessage implements Message {
+	private String sender;
+	private String message;
+	private String type;
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	
+}
